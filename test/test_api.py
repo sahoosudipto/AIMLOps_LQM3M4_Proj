123d29a73421dde61df9c7670678748f37fd1a85
Sudipto Sahoo <160700238+sahoosudipto@users.noreply.github.com>
2024-08-22 15:13:37 +0530

    Delete test directory

--- test/test_api.py
+++
@@ -1,9 +0,0 @@
-# from fastapi.testclient import TestClient
-# from movie_review_model_api.app.main import app
-
-# client = TestClient(app)
-
-# # def test_predict():
-# #     response = client.post("/predict", json={"review": "The movie was bad"})
-# #     assert response.status_code == 200
-# #     assert response.json() == {"sentiment": "Negative"}
