a38ce75d929f6bdade25df006a7336d9fd0ca71e
Sudipto Sahoo <160700238+sahoosudipto@users.noreply.github.com>
2024-08-23 21:16:10 +0530

    Update train.py

--- Movie_Review_Proj/movie_review_model/train.py
+++ Movie_Review_Proj/movie_review_model/train.py
@@ -35,18 +35,20 @@ def train_pipeline():
     # pipeline.named_steps['classifier'].model.save_pretrained(config.output.output_model_path)
     
     #     # Save the pipeline as a .pkl file
-    model_dir = Path(config.output.output_model_path)
-    model_dir.mkdir(parents=True, exist_ok=True)
+    # model_dir = Path(config.output.output_model_path)
+    # model_dir.mkdir(parents=True, exist_ok=True)
 
-    model_path = model_dir / "trained_pipeline.pkl"
-    print(f"Model path: {model_path}")
-    pipeline.named_steps['classifier'].model.save_pretrained(model_path)
+    # model_path = model_dir / "trained_pipeline.pkl"
+    # print(f"Model path: {model_path}")
+    # pipeline.named_steps['classifier'].model.save_pretrained(model_path)
 
-    # save_pipeline(pipeline_to_persist=pipeline)
-    if model_path.exists():
-        print(f"Model saved successfully at: {model_path}")
-    else:
-        print("Model save failed.")
+    # # save_pipeline(pipeline_to_persist=pipeline)
+    # if model_path.exists():
+    #     print(f"Model saved successfully at: {model_path}")
+    # else:
+    #     print("Model save failed.")
+
+    pipeline.named_steps['classifier'].model.save_pretrained("./Movie_Review_Proj/movie_review_model/trained_models/trained_pipeline.pkl")
     print("Model and pipeline saved successfully.")
 
 if __name__ == "__main__":
